fd208ed1c5b6ca2bb03251b87cd841ee69748fc2
MaximeDubost <maxime.dubost.75@gmail.com>
2020-12-03 16:52:44 +0100

    Mini projet Flask : Pré-déploiement de l'API avec les premiers endpoints (03/12/2020 - après-midi)

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MaxFlaskStore
+
+**`/!\ API EN COURS DE CRÉATION /!\`**
+
+## Routes disponibles (03/12/2020) :
+
+### Créer un produit :
+```json
+curl -d '{"name":"Produit x","description":"Description produit x","price":1,"quantity":1}' -H "Content-Type: application/json" -X POST https://max-flask-store.herokuapp.com/product
+```
+
+### Afficher tous les produits :
+```
+curl https://max-flask-store.herokuapp.com/product
+```
--- app.py
+++ app.py
@@ -3,22 +3,19 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-# Initialisation de l'application
+## Application
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Base de donnée
+## Base de donnée
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) # Database
+ma = Marshmallow(app) # ORM
 
-# Initialisation de la base de données
-db = SQLAlchemy(app)
-
-# Initialisation de l'ORM
-ma = Marshmallow(app)
-
-# Classe "Produit"
+## Classe Product 
 class Product(db.Model):
+    """ Classe "Produit" """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     description = db.Column(db.String(255))
@@ -31,11 +28,56 @@ class Product(db.Model):
         self.price = price
         self.quantity = quantity
 
-""" TODO """
+class ProductSchema(ma.Schema):
+    """ Schéma "Produit """
+    class Meta:
+        """ Schéma """
+        fields = ('id', 'name', 'description', 'price', 'quantity')
+
+
+# Initialisation du schéma
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+
+##
+## ROUTES 
+##
+
+
+## GET hello_world():
 @app.route('/', methods=['GET'])
-def get():
+def hello_world():
+    """ Affiche 'Hello World' """
     return jsonify({'msg': "Hello World"})
 
-# Lancement du serveur
+## POST add_product():
+@app.route('/product', methods=['POST'])
+def add_product():
+    """ Créer un produit """
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    product = Product(name, description, price, quantity)
+
+    db.session.add(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+## GET get_products():
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+##
+## SERVER
+##
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=True)
