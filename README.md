9b46a6054d18c76375149705b91b1c07551e2efa
MaximeDubost <maxime.dubost.75@gmail.com>
2020-12-05 19:53:44 +0100

    Mini projet Flask : Déploiement de l'API finalisée + rédaction du README.md (05/12/2020)

--- README.md
+++ README.md
@@ -1,15 +1,79 @@
 # MaxFlaskStore
 
-**`/!\ API EN COURS DE CRÉATION /!\`**
 
-## Routes disponibles (03/12/2020) :
+## **Contenu**
 
-### Créer un produit :
-```json
-curl -d '{"name":"Produit x","description":"Description produit x","price":1,"quantity":1}' -H "Content-Type: application/json" -X POST https://max-flask-store.herokuapp.com/product
+**MaxFlaskStore** est une API type e-commerce développée en Python Flask.
+
+L'API permet d'effectuer les action **CRUD** (Create, Read, Update, Delete) sur des produits d'un shop en ligne.
+
+Celle-ci dispose donc de **5** routes :
+- **`GET`** *`/product`* `:` Obtenir tous les produits
+- **`GET`** *`/product/<id>`* `:` Obtenir un produit
+- **`POST`** *`/product`* `:` Ajouter un produit
+- **`UPDATE`** *`/product/<id>`* `:` Modifier un produit
+- **`DELETE`** *`/product/<id>`* `:` Supprimer un produit
+
+L'API interagit avec une base de données **SQLite** à l'aide des ORM **SQLAlchemy** et **Marshmallow**.
+
+---
+
+## **Lancement**
+
+L'API est déployée dans un conteneur Docker sur un serveur Heroku à l'adresse suivante :
 ```
+https://max-flask-store.herokuapp.com
+```
+Celle-ci est donc actuellement déjà déployée est prête à être utilisée.
 
-### Afficher tous les produits :
+---
+
+## **Utilisation**
+
+Voici 3 manières différentes d'utiliser l'API :
+
+1. Avec la console grace à la commande `curl`
+2. Avec un `navigateur web` ( méthodes GET uniquement )
+3. Avec l'application `Postman` ( une collection Postman `MaxFlaskStore.postman_collection.json` contenant tous les endpoints est disponibles dans ce dépôt )
+
+<br>
+
+- ### **Obtenir tous les produits** ( **`GET`** *`/product`* ) 
+Commande curl :
 ```
 curl https://max-flask-store.herokuapp.com/product
+```
+URL : https://max-flask-store.herokuapp.com/product
+
+<br>
+
+- ### **Obtenir un produit** ( **`GET`** *`/product/<id>`* )
+Commande curl :
+```
+curl https://max-flask-store.herokuapp.com/product/1
+```
+URL : https://max-flask-store.herokuapp.com/product/1
+
+<br>
+
+- ### **Ajouter un produit** ( **`POST`** *`/product`* )
+Commande curl :
+```json
+curl -d '{"name":"Produit 4","description":"Description produit 4","price":4,"quantity":10}' -H "Content-Type: application/json" -X POST https://max-flask-store.herokuapp.com/product
+```
+
+<br>
+
+- ### **Modifier un produit** ( **`UPDATE`** *`/product/<id>`* )
+Commande curl :
+```json
+curl -d '{"name":"Produit 2 (updated)","description":"Description produit 2 (updated)","price":2,"quantity":10}' -H "Content-Type: application/json" -X PUT https://max-flask-store.herokuapp.com/product/2
+```
+
+<br>
+
+- ### **Supprimer un produit** ( **`DELETE`** *`/product/<id>`* )
+Commande curl :
+```
+curl -X DELETE https://max-flask-store.herokuapp.com/product/3
 ```
--- app.py
+++ app.py
@@ -3,19 +3,22 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-## Application
-app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-## Base de donnée
+app = Flask(__name__)
+
+
+# Initialisation de la base de données
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app) # Database
-ma = Marshmallow(app) # ORM
+# Initialisation des ORM
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-## Classe Product 
+
 class Product(db.Model):
-    """ Classe "Produit" """
+    """  Classe définissant un produit
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     description = db.Column(db.String(255))
@@ -23,38 +26,87 @@ class Product(db.Model):
     quantity = db.Column(db.Integer)
 
     def __init__(self, name, description, price, quantity):
+        """ Constructeur de la classe Product
+
+        Args:
+            name (str): Nom du produit
+            description (str): Description du produit
+            price (float): Prix unitaire du produit
+            quantity (int): Quantité de stock restant du produit
+        """
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
 class ProductSchema(ma.Schema):
-    """ Schéma "Produit """
+    """ Classe définissant la structure de la table Product
+    """
     class Meta:
-        """ Schéma """
+        """Classe définissant les colonnes de la table Product
+        """
         fields = ('id', 'name', 'description', 'price', 'quantity')
 
 
-# Initialisation du schéma
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
 
-##
-## ROUTES 
-##
-
-
-## GET hello_world():
 @app.route('/', methods=['GET'])
-def hello_world():
-    """ Affiche 'Hello World' """
-    return jsonify({'msg': "Hello World"})
+def home() -> Flask.response_class:
+    """ Racine de l'API
 
-## POST add_product():
+    Returns:
+        Response: Liste des routes disponibles
+    """
+    
+    return jsonify({
+        'routes': [
+            "GET /product",
+            "GET /product/<int:id>",
+            "POST /product",
+            "UPDATE /product/<int:id>",
+            "DELETE /product/<int:id>"
+        ]
+    })
+
+
+@app.route('/product', methods=['GET'])
+def get_products() -> Flask.response_class:
+    """ Obtenir la liste des produits
+
+    Returns:
+        Response: Liste des produits au format JSON
+    """
+
+    products = Product.query.all()
+
+    return jsonify(products_schema.dump(products))
+
+
+@app.route('/product/<int:_id>', methods=['GET'])
+def get_product(_id: int) -> Flask.response_class:
+    """ Obtenir un produit
+
+    Args:
+        _id (int): Identifiant du produit
+
+    Returns:
+        Response: Produit au format JSON
+    """
+
+    product = Product.query.get(_id)
+
+    return product_schema.jsonify(product)
+
+
 @app.route('/product', methods=['POST'])
-def add_product():
-    """ Créer un produit """
+def add_product() -> Flask.response_class:
+    """ Ajouter un produit
+
+    Returns:
+        Response: Produit ajouté au format JSON
+    """
 
     name = request.json['name']
     description = request.json['description']
@@ -68,16 +120,53 @@ def add_product():
 
     return product_schema.jsonify(product)
 
-## GET get_products():
-@app.route('/product', methods=['GET'])
-def get_products():
-    all_products = Product.query.all()
-    result = products_schema.dump(all_products)
-    return jsonify(result)
 
-##
-## SERVER
-##
+@app.route('/product/<int:_id>', methods=['PUT'])
+def update_product(_id: int) -> Flask.response_class:
+    """ Modifier un produit
+
+    Args:
+        _id (int): Identifiant du produit
+
+    Returns:
+        Response: Produit modifié au format JSON
+    """
+
+    product = Product.query.get(_id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.quantity = quantity
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+
+@app.route('/product/<int:_id>', methods=['DELETE'])
+def delete_product(_id: int) -> Flask.response_class:
+    """ Supprimer un produit
+
+    Args:
+        _id (int): Identifiant du produit
+
+    Returns:
+        Response: Produit supprimé au format JSON
+    """
+
+    product = Product.query.get(_id)
+
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=True)
